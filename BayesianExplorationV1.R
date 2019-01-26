482cb91fac4ea5ecf30a99cf8d1f04ed66e08043
crhuffer <crhuffer@gmail.com>
2019-01-26 16:14:02 -0500

    Update BayesianExplorationV1.R
    Minor updates while working through course.

--- BayesianExplorationV1.R
+++ BayesianExplorationV1.R
@@ -2,3 +2,23 @@
 # install.packages('LearnBayes')
 library(LearnBayes)
 
+# proportion p vector:
+p = seq(0.05, 0.95, by = 0.1); p
+
+
+# corresponding weights
+prior = c(1, 5.2, 8, 7.2, 4.6, 2.1, 0.7, 0.1, 0, 0); prior
+
+# prior probabilities of each p proportion
+prior = prior/sum(prior); prior
+
+plot(p, prior, type = "h", ylab="Prior Probability")
+
+data = c(11, 16); data
+
+# posterior densities
+post = pdisc(p, prior, data)
+post
+
+
+
